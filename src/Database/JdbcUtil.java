227d0aa89a6a8eb3176e63848b42ff6f0e92c876
NguyenVanThang2004 <154830941+NguyenVanThang2004@users.noreply.github.com>
2024-06-04 12:37:32 +0700

    update them thuc thi sql

--- src/Database/JdbcUtil.java
+++ src/Database/JdbcUtil.java
@@ -1,44 +1,54 @@
 package Database;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class JdbcUtil {
+import com.mysql.cj.jdbc.Driver;
+
+public class JDBCUtil {
 	public static Connection getConnection() {
 		Connection c = null;
-
+		
 		try {
-			// dang ki mysql  drive vs drivemanager
-			DriverManager.deregisterDriver(new com.mysql.jdbc.Driver());
-			String url  = "jdbc:mySQL://localhost:3306/ontap";
+			// Đăng ký MySQL Driver với DriverManager
+			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			
+			// Các thông số
+			String url = "jdbc:mySQL://localhost:3306/ontap";
 			String username = "root";
 			String password = "";
 			
-			// tao ket noi
-			c = DriverManager.getConnection(url,username,password);
+			// Tạo kết nối
+			c = DriverManager.getConnection(url, username, password);
 			
 		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
+		
 		return c;
 	}
-
+	
 	public static void closeConnection(Connection c) {
-
 		try {
-			
-			if(c != null)
-			{
-				c.close(); 
+			if(c!=null) {
+				c.close();
 			}
-			else 
-			{
-				
-			}
-			
 		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void printInfo(Connection c) {
+		try {
+			if(c!=null) {
+				DatabaseMetaData mtdt = c.getMetaData();
+				System.out.println(mtdt.getDatabaseProductName());
+				System.out.println(mtdt.getDatabaseProductVersion());
+			}
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
--- src/Test/TestJdbcUtil.java
+++ src/Test/TestJdbcUtil.java
@@ -1,13 +1,42 @@
 package Test;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-import Database.JdbcUtil;
+import Database.JDBCUtil;
 
-public class TestJdbcUtil {
-  public static void main(String[] args) {
-	Connection connection = JdbcUtil.getConnection();
+public class TestJDBCUtil {
+	public static void main(String[] args) {
+		
+		try {
+			// Bước 1: Tạo kết nối
+			Connection connection = JDBCUtil.getConnection();
+			
+			// Bước 2: Tạo ra đối tượng statement
+			Statement st = connection.createStatement();
+			
+			// Bước 3: Thực thi một câu lệnh SQL
+			String sql = "INSERT INTO persons(last_name, first_name, gender, dob, income)"
+					+ "VALUES (\"Tran\", \"Thi\", \"C\", \"2000-09-10\", 10000000)";
+			
+			int check = st.executeUpdate(sql);
+			
+			// Bước 4: xử lý kết quả 
+			System.out.println("Số dòng thay đổi: "+ check);
+			if(check>0) {
+				System.out.println("Thêm dữ liệu thành công!");
+			}else {
+				System.out.println("Thêm dữ liệu thất bại!");
+			}
+			
+			// Bước 5: ngắt kết nối
+			JDBCUtil.closeConnection(connection);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
 	
-	System.out.println(connection);
-}
+	
 }
